f45091a77c107d4a0763fecf6e719d5fb5c01f41
m.kulikova <m.kulikova@itransition.com>
2019-12-12 17:18:45 +0300

    Updates

---
+++ precache-manifest.3c77183fd1b36ea8f923080cd1f9d977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2768be887ce0ec5e3a80dc111339049",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "8de6a22e9516c3786972",
+    "url": "/react-chat/static/css/main.c0526bea.chunk.css"
+  },
+  {
+    "revision": "a19652d1aa260f9ee309",
+    "url": "/react-chat/static/js/2.8009d9b3.chunk.js"
+  },
+  {
+    "revision": "4ebd4c934480c725a320d9506119b042",
+    "url": "/react-chat/static/js/2.8009d9b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8de6a22e9516c3786972",
+    "url": "/react-chat/static/js/main.89a198eb.chunk.js"
+  },
+  {
+    "revision": "9a489211b6a142c8856c",
+    "url": "/react-chat/static/js/runtime-main.4a4cb897.js"
+  }
+]);
